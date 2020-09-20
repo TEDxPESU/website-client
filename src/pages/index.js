3208dd0fc136ca5ebc3804f9a156e583a05d8338
mohammed afif ahmed <afifahmed456123@gmail.com>
2020-09-20 21:22:51 +0530

    initial commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,20 @@
 import React from 'react';
+import { Link } from 'gatsby';
+import { Button, Typography } from '@material-ui/core';
+import {createMuiTheme, ThemeProvider} from '@material-ui/core';
 
-export default () => <div>Hello Frontend Masters!</div>;
+const theme = createMuiTheme({
+  typography: {
+    fontFamily: [
+      'monospace'
+    ].join(','),
+  },
+});
+
+export default () => (
+  <ThemeProvider theme={theme}>
+    <h1>Home</h1>
+    <Typography>Hello Minnesota</Typography>
+    <Link to="/about"><Button color="primary">Learn about me &rarr;</Button> </Link>
+  </ThemeProvider>
+);
